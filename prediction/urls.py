a755ad587b09b1909a9847434ba767705c2ca8d2
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-05-04 13:00:17 +0545

    specify static file path

--- prediction/urls.py
+++ prediction/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+# for static files
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='prediction-home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
