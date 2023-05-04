ac81337f96f08ad0397a8bc702ba807f61f57743
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-05-04 12:00:02 +0545

    install app 'prediction'

--- RentPredictionSystem/settings.py
+++ RentPredictionSystem/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'prediction.apps.PredictionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
