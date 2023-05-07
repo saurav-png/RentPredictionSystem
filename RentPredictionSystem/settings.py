349b9716443b8e52d36e764e3a20307a438dc44a
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-05-07 20:01:28 +0545

    change static dirs & change allowed hosts

--- RentPredictionSystem/settings.py
+++ RentPredictionSystem/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['deploySaurav.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'prediction/static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
