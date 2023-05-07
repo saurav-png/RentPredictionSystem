f45bebf736de5e8020e483062271eeebb8008916
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-05-07 18:01:48 +0545

    set DEBUG value to false and added website in allowed host

--- RentPredictionSystem/settings.py
+++ RentPredictionSystem/settings.py
@@ -28,9 +28,9 @@ load_dotenv()
 SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deploySaurav.pythonanywhere.com']
 
 
 # Application definition
